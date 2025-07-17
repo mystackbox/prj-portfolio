db9fb3bd6a7972145163e0292880ee4ffddf4ece
Yingi <yingishirinda@gmail.com>
2025-07-17 13:32:35 +0200

    addded footer component under layout

--- src/app/layout/grid-layout.module.ts
+++ src/app/layout/grid-layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GridLayoutComponent } from './grid-layout/grid-layout.component';
 import { SiteIdentityComponent } from './site-identity/site-identity/site-identity.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     GridLayoutComponent,
-    SiteIdentityComponent
+    SiteIdentityComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
