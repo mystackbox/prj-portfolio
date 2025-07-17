e07c0c53dafd2291460719500f1250984522802f
Yingi <yingishirinda@gmail.com>
2025-07-17 13:18:13 +0200

    configure grid-layout module

---
+++ src/app/layout/grid-layout.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GridLayoutComponent } from './grid-layout/grid-layout.component';
+
+@NgModule({
+  declarations: [
+    GridLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    GridLayoutComponent
+  ]
+})
+export class GridLayoutModule { }
---
+++ src/app/layout/grid-layout/grid-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-layout',
+  standalone: false,
+  templateUrl: './grid-layout.component.html',
+  styleUrl: './grid-layout.component.scss'
+})
+export class GridLayoutComponent {
+
+}
