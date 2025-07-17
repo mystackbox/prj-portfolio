2905afdf1bf511754c834d4121219727bf90d8da
Yingi <yingishirinda@gmail.com>
2025-07-17 14:11:49 +0200

    created all components required for the gridlayout

---
+++ src/app/layout/page-introduction/page-introduction/page-introduction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-introduction',
+  standalone: false,
+  templateUrl: './page-introduction.component.html',
+  styleUrl: './page-introduction.component.scss'
+})
+export class PageIntroductionComponent {
+
+}
