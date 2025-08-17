1bd7800319c6fd9816e69a39c9355c86b7118d96
Yingi <yingishirinda@gmail.com>
2025-08-17 16:52:13 +0200

    Added canDeactivate on the contact

--- src/app/features/contact/contact-routing.module.ts
+++ src/app/features/contact/contact-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './pages/contact/contact.component';
+import { UnsavedChangesGuard } from '../../core/route-guards/unsaved-changes/unsaved-change.guard';
 
-const routes: Routes = [{ path: '', component: ContactComponent,
-   data: {
-            title: 'Contact',
-            metaTags: [
-              { name: 'short-desc', content: 'what is your thought?' },
-              { name: 'page-desc', content: 'let me know your thoughts' },
-            ]
-        } 
-      }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactComponent,
+    canDeactivate: [UnsavedChangesGuard],
+    data: {
+      title: 'Contact',
+      metaTags: [
+        { name: 'short-desc', content: 'what is your thought?' },
+        { name: 'page-desc', content: 'let me know your thoughts' },
+      ],
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [UnsavedChangesGuard],
 })
-export class ContactRoutingModule { }
+export class ContactRoutingModule {}
